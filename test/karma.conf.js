38884c48890aa173cd06b370f6b7996170d7c875
Tempuss <ben3787@gmail.com>
2019-04-09 20:03:22 +0900

    Add karma Test Module

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Tue Mar 05 2019 01:19:25 GMT+0900 (GMT+09:00)
+// Generated on Tue Apr 09 2019 19:59:16 GMT+0900 (GMT+09:00)
 
 module.exports = function(config) {
   config.set({
@@ -15,7 +15,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'js/*.js'
     ],
 
 
@@ -60,7 +59,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
