ef1b8a12902285fcd1b49b06e1cbff66a1c473e0
Tempuss <ben3787@gmail.com>
2019-04-09 21:54:56 +0900

    Add protocol valide class

--- src/protocol.js
+++ src/protocol.js
@@ -1,7 +1,40 @@
 class Protocol {
 
     constructor () {
+
+        this.protocol = "";
+        this.isValid = false;
         
+        this.validProtocolList = [
+            "http",
+            "https",
+            "ftp"
+        ];
+        
+    }
+
+    validateProtocol(protocol) {
+        if (this.validProtocolList.indexOf(protocol) == -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    setProtocol(protocol) {
+        protocol = protocol.toLowerCase();
+
+        if (!this.validateProtocol(protocol)) {
+            throw "Invalid Protocol";
+        }
+
+        this.protocol = protocol;
+        this.isValid = true;
+        
+    }
+
+    getProtocol() {
+        return this.protocol;
     }
 
 }
